a80ff8b52795e0ed808548ddc584c27690479f06
Abin <abinpv95@gmail.com>
2024-01-10 18:35:59 +0530

    baseURL changed

--- src/Api/axiosInstance.js
+++ src/Api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-const baseURL = 'http://localhost:3001/';
+const baseURL = 'https://www.healthgoody.online/';
 const userBaseURL = baseURL;
 const doctorBaseURL = `${baseURL}doctor`;
 const adminBaseURL = `${baseURL}admin`;
