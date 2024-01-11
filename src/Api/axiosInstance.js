a6c04ac60c1bc93ee2f1bf47120e8cbed97636cf
Abin <abinpv95@gmail.com>
2024-01-11 10:18:20 +0530

    bugs fixed

--- src/Api/axiosInstance.js
+++ src/Api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-const baseURL = 'https://www.healthgoody.online/';
+const baseURL = 'https://www.healthgoody.online';
 const userBaseURL = baseURL;
 const doctorBaseURL = `${baseURL}doctor`;
 const adminBaseURL = `${baseURL}admin`;
