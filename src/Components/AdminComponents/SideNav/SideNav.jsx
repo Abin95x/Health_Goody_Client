7600826547b1ccebd287d5274cf5549b2b2889fe
Abin <abinpv95@gmail.com>
2024-01-16 14:52:22 +0530

    admin sidenav bug fix

--- src/Components/AdminComponents/SideNav/SideNav.jsx
+++ src/Components/AdminComponents/SideNav/SideNav.jsx
@@ -17,22 +17,22 @@ const SideNav = () => {
     <div className='bg-gray-800 text-white min-h-screen w-full sm:w-1/2 md:w-1/4 p-4'>
       <ul className='list-none p-0'>
         <li className='mb-2 hover:bg-gray-700 p-2 rounded'>
-          <Link to={'/admin/dashboard'}> <div><span className='m-5'><FontAwesomeIcon icon={faChartSimple} /></span>  DASHBOARD</div></Link>
+          <Link to={'/admin/dashboard'}> <div><span className='mx-5'><FontAwesomeIcon icon={faChartSimple} /></span>  DASHBOARD</div></Link>
         </li>
         <li className='mb-2 hover:bg-gray-700 p-2 rounded'>
-          <Link to={'/admin/appointments'}><div><span className='m-5'><FontAwesomeIcon icon={faCalendarCheck} /></span>  APPOINTMENTS</div></Link>
+          <Link to={'/admin/appointments'}><div><span className='mx-5'><FontAwesomeIcon icon={faCalendarCheck} /></span>  APPOINTMENTS</div></Link>
         </li>
         <li className='mb-2 hover:bg-gray-700 p-2 rounded'>
-          <Link to={'/admin/userlist'}><div><span className='m-5'><FontAwesomeIcon icon={faUser} /></span> USERS LIST</div></Link>
+          <Link to={'/admin/userlist'}><div><span className='mx-5'><FontAwesomeIcon icon={faUser} /></span> USERS LIST</div></Link>
         </li>
         <li className='mb-2 hover:bg-gray-700 p-2 rounded'>
-          <Link to={'/admin/doctorlist'}><div><span className='m-5'><FontAwesomeIcon icon={faUserDoctor} /></span> DOCTORS LIST</div></Link>
+          <Link to={'/admin/doctorlist'}><div><span className='mx-5'><FontAwesomeIcon icon={faUserDoctor} /></span> DOCTORS LIST</div></Link>
         </li>
         <li className='mb-2 hover:bg-gray-700 p-2 rounded'>
-          <Link to={'/admin/verifydoctors'}><div><span className='m-5'><FontAwesomeIcon icon={faCheck} /></span> VERIFY DOCTORS</div></Link>
+          <Link to={'/admin/verifydoctors'}><div><span className='mx-5'><FontAwesomeIcon icon={faCheck} /></span> VERIFY DOCTORS</div></Link>
         </li>
         <li className='mb-2 hover:bg-gray-700 p-2 rounded'>
-          <Link to={'/admin/speciality'} ><div><span className='m-5'> <FontAwesomeIcon icon={faHospital} /> </span> SPECIALITIES</div></Link>
+          <Link to={'/admin/speciality'} ><div><span className='mx-5'> <FontAwesomeIcon icon={faHospital} /> </span> SPECIALITIES</div></Link>
         </li>
       </ul>
     </div>
