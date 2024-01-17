1142f2d563331783f3e85a4bf44cfe1887b262ce
Abin <abinpv95@gmail.com>
2024-01-17 19:47:54 +0530

    speciality list design changed

--- src/Components/AdminComponents/SpeicialityList/SpecialityList.jsx
+++ src/Components/AdminComponents/SpeicialityList/SpecialityList.jsx
@@ -27,13 +27,18 @@ const SpecialityList = () => {
     setLoading(false);
 
     if (response) {
+      if (rerender) {
+        setRerender(false);
+      } else {
+        setRerender(true);
+      }
       const Toast = Swal.mixin({
         toast: true,
         position: "top",
         showConfirmButton: false,
         timer: 3000,
       });
-      setRerender(!rerender);
+      
       Toast.fire({
         icon: "success",
         title: response.data.message,
@@ -111,12 +116,10 @@ const SpecialityList = () => {
         setSlist(response?.data?.data);
         setPagination(response?.data?.pagination);
         setLoading(false);
-
       })
       .catch((error) => {
         console.log(error.message);
         setLoading(false);
-        
       });
   }, [rerender, currentPage, limit, search]);
 
@@ -144,96 +147,90 @@ const SpecialityList = () => {
 
   return (
     <>
-      {loading ? (
-        <div className=" flex justify-center min-h-screen ">
+     {loading ? (
+        <div className="flex justify-center min-h-screen">
           <span className="loading loading-ring loading-lg"></span>
         </div>
       ) : (
         <div>
-          <div>
-            <div className="overflow-x-auto ">
-              <br />
-              <div className="flex justify-between">
-                <div>
-                  <button
-                    className="btn btn-success mx-5 "
-                    onClick={() =>
-                      document.getElementById("my_modal_1").showModal()
-                    }
-                  >
-                    Add Speciality
-                  </button>
-                </div>
-                <div className=" pr-6 ">
-                  <input
-                    type="text"
-                    placeholder="search"
-                    value={search}
-                    onChange={handleChange}
-                    className="rounded-lg"
-                  />
-                </div>
+          <div className="overflow-x-auto">
+            <br />
+            <div className="flex justify-end">
+              <div>
+                <button
+                  className="btn btn-success mx-5"
+                  onClick={() => document.getElementById("my_modal_1").showModal()}
+                >
+                  Add Speciality
+                </button>
               </div>
-
-              <br />
-              <table className="table">
-                {/* head */}
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Speciality</th>
-                    <th className="px-12">Image</th>
-                    <th>Listed/Not</th>
-                    <th>Manage</th>
-                    <th>List/Unlist</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  {filteredSpeciality &&
-                    filteredSpeciality.map((specialityItem, index) => (
-                      <tr key={specialityItem._id}>
-                        <th>{index + 1}</th>
-                        <td>{specialityItem.speciality}</td>
-                        <td>
-                          <img
-                            src={specialityItem.photo}
-                            alt={specialityItem.speciality}
-                            style={{ maxWidth: "100px", maxHeight: "100px" }}
-                          />
-                        </td>
-                        <td>{specialityItem.list ? "Yes" : "No"}</td>
-
-                        <td>
-                          <button
-                            className="btn"
-                            onClick={() => handleModal(specialityItem)}
-                          >
-                            Edit
-                          </button>
-                        </td>
-                        <td>
-                          {specialityItem.list ? (
-                            <button
-                              onClick={() => handleList(specialityItem._id)}
-                              className="btn btn-error w-32"
-                            >
-                              Unlist
-                            </button>
-                          ) : (
-                            <button
-                              onClick={() => handleList(specialityItem._id)}
-                              className="btn btn-success w-32"
-                            >
-                              List
-                            </button>
-                          )}
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+              {/* <div className="pr-6">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  value={search}
+                  onChange={handleChange}
+                  className="rounded-lg"
+                />
+              </div> */}
             </div>
+
+            <br />
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Speciality</th>
+                  <th className="px-12">Image</th>
+                  <th>Listed/Not</th>
+                  <th>Manage</th>
+                  <th>List/Unlist</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredSpeciality &&
+                  filteredSpeciality.map((specialityItem, index) => (
+                    <tr key={specialityItem._id}>
+                      <th>{index + 1}</th>
+                      <td>{specialityItem.speciality}</td>
+                      <td>
+                        <img
+                          src={specialityItem.photo}
+                          alt={specialityItem.speciality}
+                          style={{ maxWidth: "100px", maxHeight: "100px" }}
+                        />
+                      </td>
+                      <td>{specialityItem.list ? "Yes" : "No"}</td>
+
+                      <td>
+                        <button
+                          className="btn btn-primary w-20"
+                          onClick={() => handleModal(specialityItem)}
+                        >
+                          Edit
+                        </button>
+                      </td>
+                      <td>
+                        {specialityItem.list ? (
+                          <button
+                            onClick={() => handleList(specialityItem._id)}
+                            className="btn btn-error w-20"
+                          >
+                            Unlist
+                          </button>
+                        ) : (
+                          <button
+                            onClick={() => handleList(specialityItem._id)}
+                            className="btn btn-success w-20"
+                          >
+                            List
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
             <br />
             <br />
             {pagination && pagination.totalPages && (
@@ -284,7 +281,7 @@ const SpecialityList = () => {
                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                   ✕
                 </button>
-                <button onClick={handleClick} className="btn btn-success">
+                <button onClick={handleClick} className="btn w-full btn-success">
                   ADD
                 </button>
 
@@ -322,7 +319,7 @@ const SpecialityList = () => {
                     accept="image/*"
                     onChange={handlePhoto}
                     type="file"
-                    className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+                    className="file-input file-input-bordered file-input-primary w-full "
                   />
 
                   <br />
@@ -330,7 +327,7 @@ const SpecialityList = () => {
 
                   <button
                     onClick={() => handleEdit(data._id)}
-                    className="btn btn-warning"
+                    className="btn btn-warning w-full"
                   >
                     Done
                   </button>
