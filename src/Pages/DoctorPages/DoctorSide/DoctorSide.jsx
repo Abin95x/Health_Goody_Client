9573bf3af824f8ee08f1ead04e22f847184c01da
Abin <abinpv95@gmail.com>
2024-01-28 19:37:52 +0530

    bug fixed

--- src/Pages/DoctorPages/DoctorSide/DoctorSide.jsx
+++ src/Pages/DoctorPages/DoctorSide/DoctorSide.jsx
@@ -12,13 +12,28 @@ const DoctorSide = () => {
           <p className='mb-5'>
             Welcome, doctors! Your dedication to healthcare is commendable. Join our community to connect with peers, access resources, and enhance your medical journey. Already a member? Log in or sign up to make a positive impact on people's lives.
           </p>
-          <Link to='/doctor/login' className='btn btn-secondary w-56'>Login</Link>
+          <Link to='/doctor/login' className=''><button
+  className="relative py-2 px-8class w-40 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
+>
+  Login
+</button>
+</Link>
           <br />
           <br />
-          <Link to='/doctor/signup' className='btn btn-accent w-56' >Signup</Link>
+          <Link to='/doctor/signup' className='' ><button
+  className="relative w-40 py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
+>
+  Signup
+</button>
+</Link>
           <br />
           <br />
-          <Link to='/' className='btn btn-primary w-56' >HOME</Link>
+          <Link to='/' className='' ><button
+  className="relative  w-40 py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
+>
+  Home
+</button>
+</Link>
 
           <div>
             <br />
